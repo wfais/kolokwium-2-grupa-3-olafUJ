177526943fd7704c7ea853830234436013279f90
Olaf Rozkrut <149374162+olafUJ@users.noreply.github.com>
2025-01-23 08:37:39 +0100

    Update main.py

--- ZADANIE4/main.py
+++ ZADANIE4/main.py
@@ -19,7 +19,11 @@ def create_and_filter_data():
     # filtered_df = ...  
 
     # return ...
-    pass
+    arr = np.random.randint(0, 101, size=(10, 2))
+    df = pd.DataFrame(arr, columns=['A', 'B'])
+    filtered_df = df[df['A'] > 50]
+
+    return filtered_df
 
 if __name__ == '__main__':
     # Przykładowe wywołanie
